df4b1845fef1f0ca6cbb8963c0a905e83630bb49
Daniel Queiroz <danielsxe@gmail.com>
2021-10-05 23:36:37 -0300

    primeiro commit

---
+++ class Foo {.js
@@ -0,0 +1,27 @@
+class Foo {
+
+    Foo(){
+
+           System.out.print(''Foo'');
+
+   }
+
+   
+
+}
+
+public class Bar extends Foo{
+
+   Bar(){
+
+           System.out.print(''Bar'');
+
+   }
+
+   public static void main(String[] args) {
+
+           Bar fb = new Bar();
+
+   }
+
+}'
---
+++ function hello(person){.ts
@@ -0,0 +1,8 @@
+function hello(person){
+    return ""Ola, "" + person;
+    }
+    let user = ""Gama"";
+    const msg = hello(user);
+    console.log(msg)"
+
+    
